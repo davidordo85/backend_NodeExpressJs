7e6c99961fab17c3628ea58a8022930ee0010f62
David Ordoñez <davidordonezescacena@hotmail.es>
2023-06-03 17:23:45 +0200

    Product controller with a GET method and added product route in app.

---
+++ routes/api/products.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+const Products = require('../../models/Products');
+
+/*
+ * GET /api/v1/products
+ */
+
+router.get('/', async function (req, res, next) {
+  try {
+    const result = await Products.find();
+    res.status(200).json({ success: true, result: result });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
