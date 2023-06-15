b5f16527d0a8c3d168659e45a416e626e10656a0
David Ordoñez <davidordonezescacena@hotmail.es>
2023-06-15 21:24:29 +0200

    Added deletion of product and verification in creating product if the user role is seller, apart from improved the way of adding categories so that they are saved correctly in the array

--- routes/api/products.js
+++ routes/api/products.js
@@ -33,6 +33,10 @@ router.get('/', async function (req, res, next) {
   }
 });
 
+/*
+ * GET /api/v1/products/:id
+ */
+
 router.get('/:id', async (req, res, next) => {
   try {
     const _id = req.params.id;
@@ -46,6 +50,10 @@ router.get('/:id', async (req, res, next) => {
     next(err);
   }
 });
+
+/*
+ * POST /api/v1/products (body)
+ */
 
 router.post('/', jwtAuth, upload.array('images'), async (req, res, next) => {
   try {
@@ -61,10 +69,77 @@ router.post('/', jwtAuth, upload.array('images'), async (req, res, next) => {
     // Agrega la fecha de creación actual como createdAt
     productData.createdAt = new Date();
 
+    // Comprueba si el rol del usuario es "seller"
+    if (req.user.role !== 'seller') {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    // Verifica si se proporcionaron categorías y las convierte en un array
+    if (productData.categories) {
+      productData.categories = productData.categories.split(',');
+    }
+
     console.log(productData);
     const product = new Products(productData);
     const productCreated = await product.save();
     res.status(201).json({ result: productCreated });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/products/:id (body)
+router.put('/:id', jwtAuth, async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    const productData = req.body;
+    const userId = req.user._id; // Obtén el ID del usuario autenticado
+
+    // Verificar si el usuario autenticado es el creador del producto
+    const product = await Products.findOne({ _id: _id, createdBy: userId });
+
+    if (!product) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const updateProduct = await Products.findOneAndUpdate(
+      { _id: _id },
+      productData,
+      {
+        new: true,
+        useFindAndModify: false,
+      },
+    );
+    // usamos { new: true } para que nos devuelva el producto actualizado
+
+    res.json({ result: updateProduct });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/products/:id
+router.delete('/:id', jwtAuth, async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+
+    // Obtén el producto por su ID
+    const product = await Products.findOne({ _id });
+
+    // Comprueba si el producto existe
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Verifica si el usuario autenticado es el creador del producto
+    if (product.createdBy !== req.user._id) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    // Borra el producto
+    await Products.deleteOne({ _id });
+
+    res.status(201).json({ result: 'Deleted product' });
   } catch (error) {
     next(error);
   }
